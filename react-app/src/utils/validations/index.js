73aece48cdeb03b6d4313b72da1bc0732e76704a
proyecto <proyecto@utn-proyecto.manope.lan>
2017-07-22 18:35:04 -0300

    validacion password registro

--- react-app/src/utils/validations/index.js
+++ react-app/src/utils/validations/index.js
@@ -4,3 +4,9 @@ export function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+
+export function isValidPassword(pass) {
+  const re = /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&\/()?¿¡$%]).{8,64})$/;
+  return re.test(pass);
+}
+
