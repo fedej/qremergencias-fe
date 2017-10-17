7ec5616d1cd45c797631fd4f5f6d87cf56ec3371
Yoga <guillermo.grillo88@gmail.com>
2017-10-16 21:26:53 -0300

    emprolijo la homepage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -6,6 +6,14 @@ import './styles.css';
 
 import Home from '../Home';
 
+
+const iconMenuProfile = require('../../assets/icons/profile_menu.png');
+const iconMenuQR = require('../../assets/icons/qr_menu.png');
+const iconMenuEmergencyData = require('../../assets/icons/emergency_data_menu.png');
+const iconMenuRecords = require('../../assets/icons/records_menu.png');
+const iconMenuEdit = require('../../assets/icons/edit_menu.png');
+const iconMenuChanges = require('../../assets/icons/changes_menu.png');
+
 class HomePage extends React.Component {
 
   static propTypes = {
@@ -19,21 +27,21 @@ class HomePage extends React.Component {
           {
             this.props.isMedico ? (
               <div>
-                <h1>Bienvenido a QR Emergencias!</h1>
+                <h1>¡Bienvenido a QR Emergencias!</h1>
                 <h2>Como médico podrás realizar las siguientes acciones desde el menú:</h2>
-                <p>Ver y editar tu perfil.</p>
-                <p>Modificar los datos de emergencia de un paciente.</p>
-                <p>Cargar estudios médicos.</p>
+                <div><span><img alt="" src={iconMenuProfile} />Ver y editar tu <strong>perfil</strong>.</span></div>
+                <div><span><img alt="" src={iconMenuEmergencyData} />Modificar los <strong>datos de emergencia</strong> de un paciente.</span></div>
+                <div><span><img alt="" src={iconMenuEdit} />Cargar <strong>estudios médicos</strong>.</span></div>
               </div>
             ) : (
               <div>
-                <h1>Bienvenido a QR Emergencias!</h1>
+                <h1>¡Bienvenido a QR Emergencias!</h1>
                 <h2>Como paciente podrás realizar las siguientes acciones desde el menú:</h2>
-                <p>Ver y editar tu perfil.</p>
-                <p>Gestionar el QR con el que podrán acceder a tus datos de emergencia.</p>
-                <p>Acceder a tu historia clínica.</p>
-                <p>Ver tus datos de emergencia.</p>
-                <p>Ver los cambios realizados por médicos sobre tu perfil.</p>
+                <div><span><img alt="" src={iconMenuProfile} />Ver y editar tu <strong>perfil</strong>.</span></div>
+                <div><span><img alt="" src={iconMenuQR} />Gestionar el <strong>QR</strong> con el que podrán acceder a tus datos de emergencia.</span></div>
+                <div><span><img alt="" src={iconMenuRecords} />Acceder a tu <strong>historia clínica</strong>.</span></div>
+                <div><span><img alt="" src={iconMenuEmergencyData} />Ver tus <strong>datos de emergencia</strong>.</span></div>
+                <div><span><img alt="" src={iconMenuChanges} />Ver los <strong>cambios realizados</strong> por médicos sobre tu perfil.</span></div>
               </div>
             )
           }
